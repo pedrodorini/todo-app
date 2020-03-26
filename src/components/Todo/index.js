1682fbdaa2b63cfca91a3661e2e35083618b43a6
Pedro Henrique Dorini <pedrodorini@MacBook-Air-de-Pedro.local>
2020-03-26 08:49:13 -0300

    add status text colors

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import classNames from 'classnames'
 
 import DropdownButton from '@components/DropdownButton'
+
+import './index.css'
 
 const Todo = ({ todo }) => {
   const { description, createdAt, done } = todo
   const mobile = window.innerWidth < 800
+  const statusClasses = classNames({
+    'todo-done': todo.done,
+    'todo-pending': !todo.done,
+  })
 
   return (
     <div className="todos-table-row">
@@ -18,7 +25,7 @@ const Todo = ({ todo }) => {
       </div>
       <div className="todos-column">
         {mobile && <p className="todos-column-description">Situação:</p>}
-        <p>{done ? 'Feito' : 'A fazer'}</p>
+        <p className={statusClasses}>{done ? 'Feito' : 'A fazer'}</p>
       </div>
       <div className="todos-column">
         <DropdownButton todo={todo} />
