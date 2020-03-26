9d64617570307aa1e6cc6ee208713bdb5a534dca
Pedro Henrique Dorini <pedrodorini@MacBook-Air-de-Pedro.local>
2020-03-25 21:38:24 -0300

    add todos basic table

--- src/pages/Todos/index.js
+++ src/pages/Todos/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 
 import Loading from '@components/Loading'
 
 import { getTodos } from '@services/todos'
 
+import './index.css'
+
 const Todos = () => {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
+  const mobile = window.innerWidth < 800
 
   useEffect(() => {
     getAllTodos()
@@ -22,16 +25,54 @@ const Todos = () => {
   }
 
   return (
-    <div>
+    <div className="todos-container">
+      <h2 className="todos-header">Afazeres</h2>
       {todos?.length > 0 ? (
-        todos.map(({ _id, description }) => (
-          <div key={_id}>
-            <p>{description}</p>
+        <Fragment>
+          <div className="todos-table">
+            <div className="todos-table-row todos-table-row-header">
+              <div className="todos-column todos-column-header">
+                <p>Tarefa</p>
+              </div>
+              <div className="todos-column todos-column-header">
+                <p>Data de criação</p>
+              </div>
+              <div className="todos-column todos-column-header">
+                <p>Situação</p>
+              </div>
+              <div className="todos-column todos-column-header">
+                <p>Ações</p>
+              </div>
+            </div>
+            {todos.map(({ _id, description, createdAt, done }) => (
+              <div className="todos-table-row" key={_id}>
+                <div className="todos-column">
+                  {mobile && <p>Tarefa</p>}
+                  <p>{description}</p>
+                </div>
+                <div className="todos-column">
+                  {mobile && <p>Data de criação</p>}
+                  <p>{new Date(createdAt)?.toLocaleDateString()}</p>
+                </div>
+                <div className="todos-column">
+                  {mobile && <p>Situação</p>}
+                  <p>{done ? 'Feito' : 'A fazer'}</p>
+                </div>
+                <div className="todos-column">
+                  <p>Ações</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))
+        </Fragment>
       ) : (
-        <p>Parece que você ainda não tem nada para fazer</p>
+        <p>
+          {loading
+            ? 'Carregando...'
+            : 'Parece que você ainda não tem nada para fazer'}
+        </p>
       )}
+
       {loading && <Loading />}
     </div>
   )
